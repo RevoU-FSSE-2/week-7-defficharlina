58d575710b6f75aa3fb0436b426201fd67c2bb29
defficharlina <136149270+defficharlina@users.noreply.github.com>
2023-08-16 15:26:17 +0700

    Update app.js

--- script/app.js
+++ script/app.js
@@ -1,26 +1,27 @@
+"use strict";
 function getFinancialsFromStorage() {
-    var financialsJSON = localStorage.getItem("financials");
+    const financialsJSON = localStorage.getItem("financials");
     return financialsJSON ? JSON.parse(financialsJSON) : [];
 }
 function addFinancial(financial) {
 }
-document.addEventListener("DOMContentLoaded", function () {
-    var myForm = document.getElementById("#myForm");
-    var financialInfo = document.getElementById("financialInfo");
+document.addEventListener("DOMContentLoaded", () => {
+    const myForm = document.getElementById("#myForm");
+    let financialInfo = document.getElementById("financialInfo");
 });
-var myForm = document.getElementById("#myForm");
-myForm.addEventListener("submit", function () {
+const myForm = document.getElementById("myForm");
+myForm.addEventListener("submit", () => {
     // ,myForm?.addEventListener("submit", (event: any) => {
     //    event?.preventDefault(); 
-    var tipeInput = document.getElementById("tipe");
-    var namaInput = document.getElementById("nama");
-    var detailsInput = document.getElementById("details");
-    var amountInput = document.getElementById("amount");
-    var tipe = tipeInput.value;
-    var nama = namaInput.value;
-    var details = detailsInput.value;
-    var amount = parseFloat(amountInput.value);
-    var financial = { tipe: tipe, nama: nama, details: details, amount: amount };
+    const tipeInput = document.getElementById("tipe");
+    const namaInput = document.getElementById("nama");
+    const detailsInput = document.getElementById("details");
+    const amountInput = document.getElementById("amount");
+    const tipe = tipeInput.value;
+    const nama = namaInput.value;
+    const details = detailsInput.value;
+    const amount = parseFloat(amountInput.value);
+    const financial = { tipe, nama, details, amount };
     addFinancialInfo(financial);
     tipeInput.value = "";
     namaInput.value = "";
@@ -29,16 +30,21 @@ myForm.addEventListener("submit", function () {
     saveFinancial(financial);
 });
 function addFinancialInfo(financial) {
-    var financialInfo = document.querySelector(".financial-info");
-    var info = document.createElement("div");
+    const financialInfo = document.querySelector(".financial-info");
+    const info = document.createElement("div");
     //info.classList.add(".financial-info");
-    info.innerHTML = "\n        <p>Tipe: ".concat(financial.tipe === 'cash in' ? 'Cash In' : 'Cash Out', "</p>\n        <p>Nama: ").concat(financial.nama, "</p>\n        <p>Details: ").concat(financial.details, "</p>\n        <p>Amount: Rp").concat(financial.amount, "</p>\n        ");
+    info.innerHTML = `
+        <p>Tipe: ${financial.tipe === 'cash in' ? 'Cash In' : 'Cash Out'}</p>
+        <p>Nama: ${financial.nama}</p>
+        <p>Details: ${financial.details}</p>
+        <p>Amount: Rp${financial.amount}</p>
+        `;
     financialInfo === null || financialInfo === void 0 ? void 0 : financialInfo.appendChild(info);
 }
 function saveFinancial(financial) {
-    var dataFinancials = getFinancialsFromStorage();
+    const dataFinancials = getFinancialsFromStorage();
     dataFinancials.push(financial);
     localStorage.setItem("financials", JSON.stringify(dataFinancials));
 }
-var saveFinancials = getFinancialsFromStorage();
+const saveFinancials = getFinancialsFromStorage();
 saveFinancials.forEach(addFinancialInfo);
